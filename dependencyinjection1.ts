f9974907cb6a821da74f38f6c5272caccd75952c
Shaka321 <141890252+Shaka321@users.noreply.github.com>
2023-09-22 21:53:53 -0400

    Create dependencyinjection1.ts

---
+++ dependencyinjection1.ts
@@ -0,0 +1,30 @@
+class EmailNotifier {
+    // Método para enviar un correo electrónico
+    sendEmail(receiver: string, subject: string, content: string) {
+        console.log(`Email sent to ${receiver} with subject "${subject}": ${content}`);
+        // Aquí podrías agregar lógica para enviar el correo electrónico real utilizando algún servicio de correo.
+    }
+}
+
+class MusicStore {
+    notifier: EmailNotifier;
+
+    // Constructor que inicializa la notificación por correo electrónico
+    constructor() {
+        this.notifier = new EmailNotifier();
+    }
+
+    // Método para notificar sobre el lanzamiento de un nuevo álbum
+    newAlbumRelease(email: string, artist: string, album: string) {
+        // Utiliza el notificador para enviar un correo electrónico de notificación
+        this.notifier.sendEmail(email, "New Album Release", `The new album ${album} by ${artist} is now available!`);
+    }
+}
+
+// Ejemplo de uso
+const musicStore = new MusicStore();
+
+// Notificar el lanzamiento de un nuevo álbum a un cliente por correo electrónico
+musicStore.newAlbumRelease("customer@example.com", "Artist Name", "Awesome Album");
+
+// Nota: El código para enviar el correo electrónico real no está implementado aquí y debe ser proporcionado según tu entorno de desarrollo y el servicio de correo que utilices.
