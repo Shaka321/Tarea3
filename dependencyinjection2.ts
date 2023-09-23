cd039b69ca6159ab21b7cc424eb29ab6f7c0544c
Shaka321 <141890252+Shaka321@users.noreply.github.com>
2023-09-22 21:55:14 -0400

    Create dependencyinjection2.ts

---
+++ dependencyinjection2.ts
@@ -0,0 +1,30 @@
+class BikeDelivery {
+    // Método para entregar un paquete utilizando una bicicleta
+    deliverPackage(packageId: string, address: string) {
+        console.log(`Delivering package ${packageId} to ${address} using a bike.`);
+        // Aquí podrías agregar lógica adicional para la entrega real del paquete con una bicicleta.
+    }
+}
+
+class PackageService {
+    deliveryMethod: BikeDelivery;
+
+    // Constructor que inicializa el método de entrega con BikeDelivery
+    constructor() {
+        this.deliveryMethod = new BikeDelivery();
+    }
+
+    // Método para enviar un paquete a una dirección específica
+    sendPackage(packageId: string, address: string) {
+        // Utiliza el método de entrega para enviar el paquete
+        this.deliveryMethod.deliverPackage(packageId, address);
+    }
+}
+
+// Ejemplo de uso
+const packageService = new PackageService();
+
+// Envía un paquete a una dirección específica utilizando BikeDelivery
+packageService.sendPackage("PKG123", "123 Main St.");
+
+// Nota: El código para la entrega real del paquete con una bicicleta no está implementado aquí y debe ser proporcionado según tu entorno de desarrollo y los detalles de la entrega logística.
